da575758b8d340e6ed47334e25e25682d5422e5c
Luyang Zhou <18929315866@163.com>
2021-05-18 02:43:51 +0800

    111

--- Student_E_prescription/settings.py
+++ Student_E_prescription/settings.py
@@ -85,23 +85,23 @@ WSGI_APPLICATION = 'Student_E_prescription.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'karlee',
-        'USER': 'adminkarlee',
-        'PASSWORD': 'anu123',
-        'HOST': '/var/run/mysql',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'karlee',
+#         'USER': 'adminkarlee',
+#         'PASSWORD': 'anu123',
+#         'HOST': '/var/run/mysql',
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

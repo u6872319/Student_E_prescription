ec46a4081ad36cd65adb7a30138cb36deecdc4b2
Luyang Zhou <18929315866@163.com>
2021-05-18 21:33:15 +0800

    “added postgres database urls”

--- Student_E_prescription/settings.py
+++ Student_E_prescription/settings.py
@@ -92,6 +92,11 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
